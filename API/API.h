c23715d652712dc3ec7508864ecc97639ee2ae22
simorgh3196 <simorgh3196@gmail.com>
2020-03-25 13:02:56 +0900

    recreate project

---
+++ API/API.h
@@ -0,0 +1,19 @@
+//
+//  API.h
+//  API
+//
+//  Created by Tomoya Hayakawa on 2020/03/25.
+//  Copyright © 2020 simorgh3196. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for API.
+FOUNDATION_EXPORT double APIVersionNumber;
+
+//! Project version string for API.
+FOUNDATION_EXPORT const unsigned char APIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <API/PublicHeader.h>
+
+
---
+++ Core/Twifter.swift
@@ -0,0 +1,9 @@
+//
+//  Twifter.swift
+//  Twifter
+//
+//  Created by Tomoya Hayakawa on 2020/03/25.
+//  Copyright © 2020 simorgh3196. All rights reserved.
+//
+
+import Foundation
