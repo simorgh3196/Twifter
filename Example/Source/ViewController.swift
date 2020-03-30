43ee74db78c12b91a9b26a9e0e9ca13207a6b750
simorgh3196 <simorgh3196@gmail.com>
2020-03-30 19:44:22 +0900

    refactor project targets

---
+++ Example/Source/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Tomoya Hayakawa on 2020/03/30.
+//  Copyright © 2020 simorgh3196. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
